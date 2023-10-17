1b08c3b3d591b17f1abd002184007b0f7ec920fb
fredferd1995 <hoffmann3733@gmail.com>
2023-10-16 22:40:20 -0700

    Add PerformSearch method

---
+++ src/search/PerformSearch.tsx
@@ -0,0 +1,28 @@
+import { UnsplashImage } from "../interfaces/Unsplash";
+import axios from 'axios';
+import {BASE_URL} from "../constants/Constants";
+import { UNSPLASH_ACCESS_KEY } from "../api/key";
+import { getErrorMessage } from "../errors/Error";
+
+export async function searchImages(query: string, page: number = 1, perPage: number = 10): Promise<UnsplashImage[]> {
+    try {
+      const response = await axios.get(`${BASE_URL}/search/photos`, {
+        params: {
+          query,
+          page,
+          per_page: perPage,
+        },
+        headers: {
+          Authorization: `Client-ID ${UNSPLASH_ACCESS_KEY}`,
+        },
+      });
+  
+      if (response.status === 200) {
+        return response.data.results;
+      } else {
+        throw new Error(`Unsplash API request failed with status code ${response.status}`);
+      }
+    } catch (error) {
+      throw new Error(`Error making Unsplash API request: ${getErrorMessage(error)}`);
+    }
+  }
